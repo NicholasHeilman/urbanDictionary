03ec9d5456d45ddbac1d2071a69d116bdb92688e
NicholasHeilman <Nicholas.Heilman@gmail.com>
2020-05-11 17:59:21 -0400

    add view and message for noResult

--- app/src/main/java/com/e/nikeurbanapp/extensions/ViewGroup.kt
+++ app/src/main/java/com/e/nikeurbanapp/extensions/ViewGroup.kt
@@ -1,10 +1,21 @@
 package com.e.nikeurbanapp.extensions
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
 fun <T: ViewDataBinding>
         ViewGroup.bind(layoutId: Int, attachToParent: Boolean = false): T =
     DataBindingUtil.inflate(LayoutInflater.from(context), layoutId, this, attachToParent)
+
+@BindingAdapter("showView")
+fun View.toggleVisiblity(show: Boolean){
+    visibility = when (show) {
+        true -> View.VISIBLE
+        false -> View.GONE
+        else -> View.INVISIBLE
+    }
+}
--- app/src/main/java/com/e/nikeurbanapp/view/SearchFragment.kt
+++ app/src/main/java/com/e/nikeurbanapp/view/SearchFragment.kt
@@ -9,17 +9,21 @@ import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.e.nikeurbanapp.R
 import com.e.nikeurbanapp.adapter.DefinitionAdapter
+import com.e.nikeurbanapp.databinding.FragmentSearchBinding
+import com.e.nikeurbanapp.extensions.toggleVisiblity
 import com.e.nikeurbanapp.model.Definition
+import com.e.nikeurbanapp.model.UrbanResponse
 import com.e.nikeurbanapp.viewModel.MainViewModel
 import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.android.synthetic.main.fragment_search.view.*
 
-
 class SearchFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: FragmentSearchBinding
     private var adapter: DefinitionAdapter? = null
 
     override fun onCreateView(
@@ -38,17 +42,20 @@ class SearchFragment : Fragment() {
             btn_Search.setOnClickListener {
                 indeterminateBar?.visibility = View.VISIBLE
                 viewModel.defineTerm(et_toolbarSearch.text.toString())
-                clearSearch()
+                iv_Clear.visibility = View.VISIBLE
             }
+
             //Clear Button
             iv_Clear.setOnClickListener {
                 et_toolbarSearch.text?.clear()
                 iv_Clear.visibility = View.INVISIBLE
             }
+
             //Filter Button
             btn_filter.setOnClickListener {
-                filterMenu(it as AppCompatImageView)
+                showFilterMenu(it as AppCompatImageView)
             }
+
             //RecyclerView
             rv_definitionList.apply {
                 layoutManager = LinearLayoutManager(context)
@@ -57,23 +64,29 @@ class SearchFragment : Fragment() {
         }
     }
 
-    private fun clearSearch(){
-            iv_Clear.visibility = View.VISIBLE
-    }
-
     override fun onStart() {
         super.onStart()
         searchInputObservable()
+//        viewModel.definitions.observe(viewLifecycleOwner) { showDefinitions(it)}
     }
 
+//    private fun showDefinitions( urbanResponse: UrbanResponse){
+//        val definitions = urbanResponse.list
+//        adapter?.submitList(definitions)
+//        binding.noResult.root.toggleVisiblity(show = false)
+//    }
     private fun searchInputObservable() {
         viewModel.definitions.observe(this, Observer {
-            adapter?.submitList(it.list)
-            indeterminateBar?.visibility = View.INVISIBLE
+                adapter?.submitList(it.list)
+                indeterminateBar?.visibility = View.INVISIBLE
+            if(it.list.isNullOrEmpty() ){
+                cv_noResult.visibility = View.VISIBLE
+            }
+
         })
     }
 
-    private fun filterMenu(filter: AppCompatImageView) {
+    private fun showFilterMenu(filter: AppCompatImageView) {
         viewModel.definitions.value?.list?.let {
             PopupMenu(filter.context, filter).apply {
                 menuInflater.inflate(R.menu.filter_menu, menu)
@@ -81,8 +94,8 @@ class SearchFragment : Fragment() {
                     val sortedList: List<Definition> = when(item.itemId) {
                         R.id.up_votes_ascending -> { it.toMutableList().apply { sortBy { it.thumbsUp } }.toList() }
                         R.id.up_votes_descending -> { it.toMutableList().apply { sortByDescending { it.thumbsUp } }.toList() }
-                        R.id.down_votes_ascending ->  { it.toMutableList().apply { sortBy { it.thumbsDown } }.toList() }
-                        R.id.down_votes_descending ->{ it.toMutableList().apply { sortByDescending { it.thumbsDown } }.toList() }
+                        R.id.down_votes_ascending -> { it.toMutableList().apply { sortBy { it.thumbsDown } }.toList() }
+                        R.id.down_votes_descending -> { it.toMutableList().apply { sortByDescending { it.thumbsDown } }.toList() }
                         else -> it
                     } as List<Definition>
                     adapter?.submitList(sortedList)
@@ -91,23 +104,7 @@ class SearchFragment : Fragment() {
             }.show()
         }
     }
+
 }
-
-
-//        popupMenu.menuInflater.inflate(R.menu.filter_menu, popupMenu.menu)
-//        popupMenu.setOnMenuItemClickListener{ item ->
-//            when(item.itemId) {
-//                R.id.up_votes_ascending ->
-//                    Toast.makeText(context, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
-//                R.id.up_votes_descending ->
-//                    Toast.makeText(context, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
-//                R.id.down_votes_ascending ->
-//                    Toast.makeText(context, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
-//                R.id.down_votes_descending ->
-//                    Toast.makeText(context, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
-//            }
-//            true
-//        }
-//        popupMenu.show()
 
 

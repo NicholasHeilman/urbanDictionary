40e8d2716c3f81963bf29f48c7e09260b5f15b01
Nicholas Heilman <Nicholas.heilman@gmail.com>
2022-01-18 14:12:00 -0600

    update gradle and data/viewBinding

--- app/src/main/java/com/e/nikeurbanapp/view/SearchFragment.kt
+++ app/src/main/java/com/e/nikeurbanapp/view/SearchFragment.kt
@@ -6,25 +6,31 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.e.nikeurbanapp.R
 import com.e.nikeurbanapp.adapter.DefinitionAdapter
+import com.e.nikeurbanapp.databinding.FragmentSearchBinding
 import com.e.nikeurbanapp.model.Definition
 import com.e.nikeurbanapp.viewModel.MainViewModel
-import kotlinx.android.synthetic.main.fragment_search.*
-import kotlinx.android.synthetic.main.fragment_search.view.*
-import kotlinx.android.synthetic.main.snippet_toolbar.*
-import kotlinx.android.synthetic.main.snippet_toolbar.view.*
+//import kotlinx.android.synthetic.main.fragment_search.*
+//import kotlinx.android.synthetic.main.fragment_search.view.*
+//import kotlinx.android.synthetic.main.snippet_toolbar.*
+//import kotlinx.android.synthetic.main.snippet_toolbar.view.*
 
 class SearchFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels()
     private var adapter: DefinitionAdapter? = null
     var query: CharSequence? = ""
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
 
     private val textWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {
@@ -38,15 +44,15 @@ class SearchFragment : Fragment() {
         override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
             val isBackSpace = query?.length ?: -1 >= s?.length ?: -1
             query = s.toString()
-            rv_definitionList.visibility = View.VISIBLE
-            iv_Clear.visibility = View.VISIBLE
-            welcomeView.visibility = View.GONE
-            noResultView.visibility = View.GONE
+            binding.rvDefinitionList.visibility = View.VISIBLE
+            binding.toolbar.ivClear.visibility = View.VISIBLE
+            binding.welcomeView.cvWelcome.visibility = View.GONE
+            binding.noResultView.cvNoResult.visibility = View.GONE
 
             s?.let {
                 if (it.isNotEmpty() && !isBackSpace) {
                     viewModel.defineTerm(it.toString())
-                    indeterminateBar?.visibility = View.VISIBLE
+                    binding.indeterminateBar?.visibility = View.VISIBLE
                 }
             }
         }
@@ -56,30 +62,37 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+//        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         adapter = DefinitionAdapter()
-        activity?.et_toolbarSearch?.addTextChangedListener(textWatcher)
+
+        binding.toolbar.etToolbarSearch.addTextChangedListener(textWatcher)
+//        activity?.et_toolbarSearch?.addTextChangedListener(textWatcher)
         view.apply {
+            val ivClear: ImageView = findViewById(R.id.iv_Clear)
 
             //Clear Button
-            iv_Clear.setOnClickListener {
-                et_toolbarSearch.text?.clear()
-                welcomeView.visibility = View.VISIBLE
-                rv_definitionList.visibility = View.GONE
-                iv_Clear.visibility = View.INVISIBLE
+            ivClear.setOnClickListener {
+                binding.toolbar.etToolbarSearch.text?.clear()
+                binding.welcomeView.cvWelcome.visibility = View.VISIBLE
+                binding.rvDefinitionList.visibility = View.GONE
+                binding.toolbar.ivClear.visibility = View.INVISIBLE
             }
 
             //Filter Button
-            btn_filter.setOnClickListener {
+            binding.toolbar.btnFilter.setOnClickListener {
                 showFilterMenu(it as AppCompatImageView)
             }
 
             //RecyclerView
-            rv_definitionList.apply {
+            binding.rvDefinitionList.apply {
                 layoutManager = LinearLayoutManager(context)
                 adapter = this@SearchFragment.adapter
             }
@@ -95,9 +108,9 @@ class SearchFragment : Fragment() {
     private fun searchInputObservable() {
         viewModel.definitions.observe(this, Observer {
             adapter?.submitList(it.list)
-            indeterminateBar?.visibility = View.INVISIBLE
+            binding.indeterminateBar?.visibility = View.INVISIBLE
             if (it.list.isNullOrEmpty()) {
-                noResultView.visibility = View.VISIBLE
+                binding.noResultView.cvNoResult.visibility = View.VISIBLE
             }
         })
     }
@@ -129,4 +142,9 @@ class SearchFragment : Fragment() {
         }
     }
 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
